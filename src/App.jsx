59584f3d0932d884028b4148f10ea0af2ae5a1eb
mukitur <mukit.inway@gmail.com>
2023-09-13 16:15:16 +0600

    inalize

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './Component/Home/Home';
 
 function App() {
   const [carts, setCarts] = useState([]);
+  const [remaining, setRemaining] = useState(0);
+  const [totalCost, setTotalCost] = useState(0);
 
   const handleAddToCart = (avenger) => {
     // console.log('cart added soon', avenger);
@@ -19,16 +21,23 @@ function App() {
         count = count + item.salary;
       });
       // console.log(count);
+      const totalRemaining = 20000 - count;
 
-      const newAvenger = [...carts, avenger];
-      setCarts(newAvenger);
+      if (count > 20000) {
+        return alert('Budjet over');
+      } else {
+        setTotalCost(count);
+        setRemaining(totalRemaining);
+        const newAvenger = [...carts, avenger];
+        setCarts(newAvenger);
+      }
     }
   };
   return (
     <>
       <div className="flex">
         <Home handleAddToCart={handleAddToCart} />
-        <Carts carts={carts} />
+        <Carts carts={carts} remaining={remaining} totalCost={totalCost} />
       </div>
     </>
   );
--- src/Component/Carts/Carts.jsx
+++ src/Component/Carts/Carts.jsx
@@ -1,12 +1,16 @@
 import Cart from '../Cart/Cart';
 
-const Carts = ({ carts }) => {
+const Carts = ({ carts, remaining, totalCost }) => {
   //   console.log(carts);
   return (
     <div className="w-1/3">
       <h3>Total Select : {carts.length}</h3>
+      <h5>Total Budjet: $20000</h5>
+      <p> Remaining: {remaining}</p>
+      <h5>Total Cost: {totalCost}</h5>
+
       {carts.map((cart, idx) => (
-        <Cart key={idx} cart={cart} />
+        <Cart key={idx} remaining={remaining} cart={cart} />
       ))}
     </div>
   );
